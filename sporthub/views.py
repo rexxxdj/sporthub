778616ea70f0f30c26981bec4e267f925606a4ae
Ruslan Tokar <djrexxx87@gmail.com>
2022-07-20 14:18:59 +0300

    first commit

---
+++ sporthub/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    return redirect('/login')
+
+'''def login(request):
+    return render(request, 'login.html', {})
+'''
+def register(request):
+    return render(request, 'register.html', {})
+
+def forgot_password(request):
+    return render(request, 'forgot-password.html', {})
+
+def settings(request):
+    return render(request, 'settings.html', {})
